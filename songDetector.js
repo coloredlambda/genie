233674e9f40225a9f43d74dd8a4f52b3eb76f635
Raaj <raajable@gmail.com>
2018-01-20 14:54:45 +0000

    Module to detect playing song

--- songDetector.js
+++ songDetector.js
@@ -1,19 +1,27 @@
 const dbus = require('dbus');
 
+// I put the dbus service details in arrays so we can support more players in future
+const service = ['org.gnome.Rhythmbox3'];
+const servicePath = ['/org/mpris/MediaPlayer2'];
+const serviceInterface = ['org.mpris.MediaPlayer2.Player'];
+
+// Function to detect running song on OS
 const detectSong = () => {
     const bus = new dbus.getBus('session');
-    let service = ['org.gnome.Rhythmbox3'];
-    let servicePath = ['/org/mpris/MediaPlayer2'];
-    let serviceInterface = ['org.mpris.MediaPlayer2.Player'];
-
-
+    console.log('Detecting song');
     bus.getInterface(service[0], servicePath[0], serviceInterface[0], (err, player) => {
         if(err){
             throw err;
         }else{
-            return function (){
-                console.log(player.PlayPause);
-            }
+            player.getProperties((err, properties) => {
+                if(err){
+                    bus.disconnect();
+                    throw err;
+                }else{
+                    console.log(properties.Metadata);
+                    bus.disconnect();
+                }
+            })
         }
     });
 
