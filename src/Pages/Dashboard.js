2a9c2c18a5bea9f5bfc5b978fb5170aae03732e4
Raaj <raajable@gmail.com>
2018-01-20 13:46:06 +0000

    Added illustration image

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -3,7 +3,8 @@ import { SearchField, Button } from 'react-desktop';
 import './Dashboard.css';
 
 //Logo Image
-import logo from '../logo.png';
+import logo from '../Images/logo.png';
+import searchImage from '../Images/search.svg';
 
 class Dashboard extends React.Component{
     render(){
@@ -11,11 +12,19 @@ class Dashboard extends React.Component{
             <div className="App">
                 <header className="App-header">
                     <img src={logo} height='30px' width='30px'/>
-                    <h1 className="App-title">Genie Lyrics</h1>
+                    <h1 className="App-title">Genie</h1>
+                    <h3>Lyrical nerd</h3>
                 </header>
 
                 <div className='Search'>
+                    <img
+                        src={searchImage}
+                        width='200px'
+                        height='100px'
+                    />
+
                     <h1>Search Lyrics Manually</h1>
+
                     <SearchField
                         size='15px'
                         placeholder='The Chainsmokers - Closer'
